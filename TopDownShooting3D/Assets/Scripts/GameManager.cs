2a243903e0290a93406cf5c135a5302fd78127e8
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-08-26 16:42:43 +0900

    250826
    레벨 시작 시 아이템 배치 및 방해물 생성 관련 데이터 추가

--- TopDownShooting3D/Assets/Scripts/GameManager.cs
+++ TopDownShooting3D/Assets/Scripts/GameManager.cs
@@ -10,12 +10,36 @@ public class GameManager : MonoBehaviour
 
     public GameObject endGamePanel;
 
+    //아이템 박스 프리팹 및 맵 장애물 및 아이템 스폰 장소 관련 모음 변수들 
+
+    public GameObject itemBox;
+
+    public List<GameObject> obstacleList = new List<GameObject>();
+
+    public List<GameObject> boxSpawnAreaList = new List<GameObject>();
+
+    //////////////////////-------------------------------------------------------
+
     private void Awake()
     {
         if(Instance == null)
         {
             Instance = this;
         }
+    }
+
+    private void Start()
+    {
+        
+        int rand1 = Random.Range(0, obstacleList.Count);
+
+        obstacleList[rand1].SetActive(true);
+
+        for(int i =0; i < boxSpawnAreaList.Count; i++)
+        {
+           Instantiate(itemBox, boxSpawnAreaList[i].transform.position , Quaternion.identity);
+        }
+
     }
 
     public void RestartGame()
